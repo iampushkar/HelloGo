e9be580d55e763a485a575ade76068a906ba7ec5
iampushkar <m4manishpushkar@gmail.com>
2023-10-02 04:47:53 +0000

    go syntaxes

--- main.go
+++ main.go
@@ -5,6 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello, Pushkar!")
-  fmt.Println("Welcome with Go")
+	fmt.Println("Hello World in GO!")
+  var name string = "Manish Pushkar"
+  fmt.Println("My name is " + name)
+
+  cards := []string {"Ace of Diamonds", newCard()}
+  cards = append(cards, "Six of Spades")
+
+  fmt.Println(cards)
+
+  for i, card := range cards {
+    fmt.Println(i, card)
+  }
+}
+
+func newCard() string {
+  return "Five of Diamonds"
 }
