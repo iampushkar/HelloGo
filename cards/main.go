40e1561d3d5322958e55db61a1dc906a4152ab40
iampushkar <m4manishpushkar@gmail.com>
2023-10-02 05:29:08 +0000

    Adding deck of 52 cards

--- cards/main.go
+++ cards/main.go
@@ -1,24 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
+// import "fmt"
 
 func main() {
-	fmt.Println("Hello World in GO!")
-  var name string = "Manish Pushkar"
-  fmt.Println("My name is " + name)
+	cards := newDeck()
 
-  cards := []string {"Ace of Diamonds", newCard()}
-  cards = append(cards, "Six of Spades")
-
-  fmt.Println(cards)
-
-  for i, card := range cards {
-    fmt.Println(i, card)
-  }
-}
-
-func newCard() string {
-  return "Five of Diamonds"
+	deck.print(cards)
 }
