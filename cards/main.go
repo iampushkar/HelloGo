be777415352591165f84cb99a7c93d95f54020fa
iampushkar <m4manishpushkar@gmail.com>
2023-10-02 09:51:09 +0000

    saving to file

--- cards/main.go
+++ cards/main.go
@@ -1,12 +1,6 @@
 package main
 
-// import "fmt"
-
 func main() {
 	cards := newDeck()
-	// cards.print()
-
-	hand, remainingCards := deal(cards, 5)
-	hand.print()
-	remainingCards.print()
+	cards.saveToFile("my_cards")
 }
